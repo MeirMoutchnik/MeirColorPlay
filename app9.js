9ee8c6737548d25dbdcd1ade173d4c50a4341209
Meir Moutchnik <moutchnikm@gmail.com>
2025-07-13 16:48:43 +0300

    adding color play 9

---
+++ app9.js
@@ -0,0 +1,25 @@
+const grids = document.querySelectorAll(".grid-b");
+const btn = document.getElementById("next-color");
+
+let j = 0;
+
+const colorChange = () => {
+  for (const grid of grids) {
+    const items = grid.querySelectorAll(".item");
+    let k = 0;
+    for (const item of items) {
+      item.style.backgroundColor = `hsl(${j}, 100%, ${90 - k}%)`;
+      k += 5;
+    }
+  }
+  j += 30;
+};
+
+colorChange();
+
+const interval = setInterval(colorChange, 2000);
+// const clear = clearInterval(interval);
+const stopColorChange = () => clearInterval(interval);
+btn.addEventListener("click", stopColorChange);
+
+// setTimeout(clear, 10000);
